e90b8f2e3b3ca9d97673796cf930e258381c8536
innabahamya <i.kurashevich@gmail.com>
2024-03-11 10:36:06 +0400

    add SuperheroCard

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@
 import './App.css';
 
 import Header from './components/Header';
-import Card from './components/Card';
+
 
 
 function App() {
   return (
     <div className="App">
      <Header/>
-     <Card/>
+     
     </div>
 
     
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,20 +1,12 @@
-function Card(props) {
-   
-const {title, price, description, imgLink} = this.props;
+const SuperheroCard = ({ name, image, powers, description }) => {
+  return (
+    <div className="superhero-card">
+      <img src={image} alt={name} />
+      <h2>{name}</h2>
+      <p><strong>Powers:</strong> {powers.join(', ')}</p>
+      <p>{description}</p>
+    </div>
+  );
+};
 
-        return (
-            <div className="card">
-                <div className="card-body">
-                    <img src={imgLink} alt={title}/>
-                    <h4 className="card-title">{title}</h4>
-                    <p className="card-text">{description}</p>
-                </div>
-                <div className="card-footer">
-                    <span className="card-price">${price}</span>
-                    <button className="card-add">Заказать</button>
-                </div>
-            </div>
-        );
-    }
-
-    export default Card
+export default SuperheroCard;
